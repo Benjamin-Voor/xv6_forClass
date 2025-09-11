9685fe6f81bceed482ed37c50597e9f0a9227839
Benjamin-Voor <benjamin.a.voor@outlook.com>
2025-09-11 17:33:39 -0400

    Fixed Khalee's branch
    Khalee said he accidentally put one "xv6_patched" folder inside the other folder, like a Russian nesting doll ("Matryoshka doll"). I stored the inner folder, deleted the outer folder, then pasted the inner folder back in.
    
    Co-Authored-By: Khalee242 <151599422+Khalee242@users.noreply.github.com>

--- xv6_patched/kernel/syscall.c
+++ xv6_patched/kernel/syscall.c
@@ -103,8 +103,6 @@ static int (*syscalls[])(void) = {
 [SYS_wait]    sys_wait,
 [SYS_write]   sys_write,
 [SYS_uptime]  sys_uptime,
-[SYS_printpid] sys_uptime,
-[SYS_numtimesgetpid] sys_numtimesgetpid,
 };
 
 // Called on a syscall trap. Checks that the syscall number (passed via eax)
